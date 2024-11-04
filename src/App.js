0b8233bcb10a03b281327239eb3690370b7dfa13
Sandeep Jha <sandeepjhavc@gmail.com>
2024-11-04 14:46:03 +0530

    project1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Services from './components/Services'
+
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss"
+import "./styles/footer.scss"
+import "./styles/contact.scss"
+import "./styles/mediaquery.scss"
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="*" element={<div>error</div>} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <Footer>
+            <div>
+                <h1>Sandeep Jha</h1>
+                <p>@all rights reserved</p>
+            </div>
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+                    <a href="/#" target={"blank"}>Instagram</a>
+                    <a href="/#" target={"blank"}>Youtube</a>
+                    <a href="/#" target={"blank"}>Github</a>
+                </div>
+            </div>
+        </Footer>
+    )
+}
+
+export default Footer
